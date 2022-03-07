338c0bb7665ac4b68a5c73abd016812164daf409
Victor Dorado <vdorado@hedyla.com>
2022-03-07 22:15:42 +0100

    New version

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # vdf-website-2.0
+
 VDF portfolio website done in python.
 
 Para correlo y que sea accesible desd ela web hay que: flask run --host=0.0.0.0
+
+export FLASK_ENV=development
+export FLASK_APP=app
--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
